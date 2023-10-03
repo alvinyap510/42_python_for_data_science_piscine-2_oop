f69f67d516086c686127d53c014a2fd7c88b368b
Alvin Yap <leaf.alvin@gmail.com>
2023-10-04 04:00:37 +0800

    feat: add ex03

---
+++ ex03/ft_calculator.py
@@ -0,0 +1,52 @@
+'''
+This exercise is about learning operator overloading.
+'''
+
+
+class calculator:
+
+    def __init__(self, num_array: list[float]):
+        '''
+        Constructor function of calculator class.
+        '''
+        self.num_array = num_array
+
+    def __add__(self, object) -> None:
+        '''
+        Overriding the build in __add__, and called when "+" operator
+        is used.
+        '''
+        assert isinstance(object, (int, float)
+                          ), "Operator must followed by int or float"
+        self.num_array = [n + object for n in self.num_array]
+        print(self.num_array)
+
+    def __mul__(self, object) -> None:
+        '''
+        Overriding the build in __mul__, and called when "*" operator
+        is used.
+        '''
+        assert isinstance(object, (int, float)
+                          ), "Operator must followed by int or float"
+        self.num_array = [n * object for n in self.num_array]
+        print(self.num_array)
+
+    def __sub__(self, object) -> None:
+        '''
+        Overriding the build in __sub__, and called when "-" operator
+        is used.
+        '''
+        assert isinstance(object, (int, float)
+                          ), "Operator must followed by int or float"
+        self.num_array = [n - object for n in self.num_array]
+        print(self.num_array)
+
+    def __truediv__(self, object) -> None:
+        '''
+        Overriding the build in __truediv__, and called when "/" operator
+        is used.
+        '''
+        assert isinstance(object, (int, float)
+                          ), "Operator must followed by int or float"
+        self.num_array = [n / object for n in self.num_array]
+        print(self.num_array)
